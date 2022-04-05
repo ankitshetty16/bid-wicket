da4af6afbae7f4df73d43203455f3f281d43d5e4
ankitshetty16 <ankitshetty16@gmail.com>
2022-04-04 20:41:16 -0400

    Added Bidwicket-app base code

---
+++ bidwicket-app/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.use(express.static('src'));
+app.use(express.static('../bidwicket-contract/build/contracts'));
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.listen(3000,function(){
+    console.log('Bidwicket application is running on port 3000!');
+});
